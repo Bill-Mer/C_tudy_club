bf475b5a44f31afbda93166aaf37388c4d61bf8c
Bill-Mer <billmermiklis@gmail.com>
2022-03-25 10:54:50 +0200

    Update 4_diagonal_sum.c

--- arrays/4_diagonal_sum.c
+++ arrays/4_diagonal_sum.c
@@ -25,6 +25,7 @@ int main() {
     printf(" = %d\n", sum);
 
     // secondary diagonal
+    sum = 0;
     for (int i=0, j=2; i<3 && j>=0; i++, j--) {
         // add array element to the sum and print it
         sum += array[i][j];
